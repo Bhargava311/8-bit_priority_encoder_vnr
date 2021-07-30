b7c381b1aa8838452f2b0fe9f6aa6f155f8ae093
Bhargava311 <86703317+Bhargava311@users.noreply.github.com>
2021-07-30 19:18:25 +0530

    Add files via upload

---
+++ pirorityencoder.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:26:44 07/29/2021 
+// Design Name: 
+// Module Name:    pirorityencoder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module pirorityencoder(d_out, d_in);
+   output [2:0] d_out;
+   input [7:0] d_in ;
+
+assign d_out = (d_in[7] ==1'b1 ) ? 3'b111:
+               (d_in[6] ==1'b1 ) ? 3'b110:
+               (d_in[5] ==1'b1 ) ? 3'b101:
+               (d_in[4] ==1'b1) ? 3'b100:
+               (d_in[3] ==1'b1) ? 3'b011:
+               (d_in[2] ==1'b1) ? 3'b010:
+               (d_in[1] ==1'b1) ? 3'b001:
+               (d_in[0] ==1'b1) ? 3'b000: 3'bxxx;
+
+endmodule
---
+++ priorityencoder_tb.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   18:29:46 07/29/2021
+// Design Name:   pirorityencoder
+// Module Name:   E:/pe/priorityencoder_tb.v
+// Project Name:  pe
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: pirorityencoder
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module priorityencoder_tb;
+
+   reg [7:0] d_in;
+   wire[2:0] d_out;
+
+   prio_enco_8x3 u1 (.d_out(d_out), .d_in(d_in) );
+
+   initial
+     begin
+    d_in=8'b11001100;
+    #10;
+    d_in=8'b01100110;
+    #10;
+    d_in=8'b00110011; 
+    #10;
+    d_in=8'b00010010;
+    #10;
+    d_in=8'b00001001;
+    #10;
+    d_in=8'b00000100;
+    #10;
+    d_in=8'b00000011;
+    #10;
+    d_in=8'b00000001;
+    #10;
+    d_in=8'b00000000;
+    # 10; 
+    $stop;
+     end // initial begin
+ endmodule
+
